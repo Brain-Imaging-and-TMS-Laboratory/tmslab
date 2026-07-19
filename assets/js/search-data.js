592cfa9aa858ace7a056cb491f378e674369bc10
hangbinzhang <65265314+hangbinzhang@users.noreply.github.com>
2026-07-19 17:51:59 +0000

    Deploying to gh-pages from @ Brain-Imaging-and-TMS-Laboratory/tmslab@cfe1353d299d2cfac44b0d17872e643485959ae7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,29 +58,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/tmslab/contact/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-        
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
-      },{id: "news-three-new-publications-chou-et-al-2020-clin-neurophysiol-on-tms-induced-seizure-cases-warren-et-al-2020-brain-connect-on-amygdala-resting-state-fmri-and-chou-et-al-2020-neurobiol-aging-a-meta-analysis-of-rtms-effects-on-mci-and-ad",
+        },{id: "news-three-new-publications-chou-et-al-2020-clin-neurophysiol-on-tms-induced-seizure-cases-warren-et-al-2020-brain-connect-on-amygdala-resting-state-fmri-and-chou-et-al-2020-neurobiol-aging-a-meta-analysis-of-rtms-effects-on-mci-and-ad",
           title: 'Three new publications! (Chou et al. 2020, Clin. Neurophysiol. on TMS-induced seizure cases,...',
           description: "",
           section: "News",},{id: "news-congratulations-to-rudolph-rodriguez-for-receiving-the-college-of-science-galileo-circle-scholarship-and-for-winning-third-place-in-the-undergraduate-research-poster-presentation-at-the-2020-waeso-student-research-conference",
